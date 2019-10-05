569a40f2e8c952979df4c2f4e9c23b55a2ff5a73
AntiMux <max23man24@gmail.com>
2019-10-05 20:07:38 +0300

    added files

---
+++ app.js
@@ -0,0 +1,62 @@
+
+const btn = document.querySelector ('.contaner button');
+var showText =document.querySelector ('.contaner p');   
+var videoLink = document.getElementById("theVideo");
+
+const apiKey = "AIzaSyCt6BCr5SN07CLxm83e5TfX1O06ccbPlI8";
+
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+
+
+function api_url(){
+    let url='https://www.googleapis.com/youtube/v3/search?key='+apiKey+'&maxResults=50&part=snippet&type=video&q='+makeid(4);
+    return url;
+
+};
+
+
+
+// const api_json = async () => {
+//     const response = await fetch(api_url());
+//     const json = await response.json();
+//     return json;
+
+// };
+
+
+
+const api_json = () => {
+    var value = $.getJSON({
+        url: api_url(),
+        async: false
+
+    }).responseText;
+    return JSON.parse(value);
+};
+
+
+
+let data = api_json();
+
+console.log(data.items[0])
+
+
+
+
+const addText = () => {
+    btn.addEventListener ('click', function() {
+        //videoLink.src ="https://www.youtube.com/embed/"+linkID;
+        //console.log(api_url());
+    });
+};
+
+addText();
