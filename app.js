391a9e45e379ed459f699d37985c8353a40af868
Hannister <hanny.y@gmail.com>
2019-10-13 21:28:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-
-const btn = document.querySelector ('.contaner button');
-var showText =document.querySelector ('.contaner p');   
+const btn = document.querySelector('.contaner button');
+var showText = document.querySelector('.contaner p');
 var videoLink = document.getElementById("theVideo");
 
 
 
-const apiKey = "AIzaSyCt6BCr5SN07CLxm83e5TfX1O06ccbPlI8";
+const apiKey = "AIzaSyCB-ju7msA92oEwsNiwbbqlKPtAVi_khF0";
 var random;
 
 
@@ -13,19 +12,19 @@ var random;
 
 
 function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-    document.cookie = "random ="+result;
+    document.cookie = "random =" + result;
     return result;
- }
+}
 
 
-function api_url(){
-    let url='https://www.googleapis.com/youtube/v3/search?key='+apiKey+'&maxResults=50&part=snippet&type=video&q='+makeid(4);
+function api_url() {
+    let url = 'https://www.googleapis.com/youtube/v3/search?key=' + apiKey + '&maxResults=50&part=snippet&type=video&q=' + makeid(4);
     return url;
 };
 
@@ -39,19 +38,19 @@ const api_json = () => {
 };
 
 
-function get_all_ids(){
+function get_all_ids() {
     let array = [];
     let array_clean = [];
     let json = api_json().items;
-    for (video in json){
+    for (video in json) {
         array.push(json[video]["id"]["videoId"]);
     }
     return array;
 };
 
 const addText = () => {
-    btn.addEventListener ('click', function() {
-        videoLink.src ="https://www.youtube.com/embed/"+ getVideoId()+"?&autoplay=1";
+    btn.addEventListener('click', function () {
+        videoLink.src = "https://www.youtube.com/embed/" + getVideoId() + "?&autoplay=1";
         console.log(splitCookieToArray());
     });
 };
