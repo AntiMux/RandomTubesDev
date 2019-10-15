df3152f231cdbd96373de561693c156ab3a28586
Hannister <hanny.y@gmail.com>
2019-10-15 23:50:29 +0300

    add animation to button

--- app.js
+++ app.js
@@ -1,58 +1,55 @@
-const btn = document.querySelector('.contaner button');
-var showText = document.querySelector('.contaner p');
+const btn = document.querySelector(".contaner button");
+var showText = document.querySelector(".contaner p");
 var videoLink = document.getElementById("theVideo");
-
-
 
 const apiKey = "AIzaSyCB-ju7msA92oEwsNiwbbqlKPtAVi_khF0";
 var random;
 
-
-
-
-
 function makeid(length) {
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    document.cookie = "random =" + result;
-    return result;
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  document.cookie = "random =" + result;
+  return result;
 }
 
-
 function api_url() {
-    let url = 'https://www.googleapis.com/youtube/v3/search?key=' + apiKey + '&maxResults=50&part=snippet&type=video&q=' + makeid(4);
-    return url;
-};
+  let url =
+    "https://www.googleapis.com/youtube/v3/search?key=" +
+    apiKey +
+    "&maxResults=50&part=snippet&type=video&q=" +
+    makeid(4);
+  return url;
+}
 
 const api_json = () => {
-    var value = $.getJSON({
-        url: api_url(),
-        async: false
-
-    }).responseText;
-    return JSON.parse(value);
+  var value = $.getJSON({
+    url: api_url(),
+    async: false
+  }).responseText;
+  return JSON.parse(value);
 };
-
 
 function get_all_ids() {
-    let array = [];
-    let array_clean = [];
-    let json = api_json().items;
-    for (video in json) {
-        array.push(json[video]["id"]["videoId"]);
-    }
-    return array;
-};
+  let array = [];
+  let array_clean = [];
+  let json = api_json().items;
+  for (video in json) {
+    array.push(json[video]["id"]["videoId"]);
+  }
+  return array;
+}
 
 const addText = () => {
-    btn.addEventListener('click', function () {
-        videoLink.src = "https://www.youtube.com/embed/" + getVideoId() + "?&autoplay=1";
-        console.log(splitCookieToArray());
-    });
+  btn.addEventListener("click", function() {
+    videoLink.src =
+      "https://www.youtube.com/embed/" + getVideoId() + "?&autoplay=1";
+    console.log(splitCookieToArray());
+  });
 };
 
 addText();
